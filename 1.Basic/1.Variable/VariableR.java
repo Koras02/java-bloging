e4f0ef44ae667fd5475aecb69e38217267e7f90e
Koras02 <wjdgh0727@gmail.com>
2025-02-25 02:41:20 +0900

    1.Data&Variables Type

---
+++ 1.Basic/1.Variable/VariableR.java
@@ -0,0 +1,14 @@
+
+public class VariableR {
+
+    public static void main(String[] args) {
+        int age = 25; // age 변수를 25로 초기화
+        double salary = 50000.0; // salary 변수를 50000.0으로 초기화
+        String name = "Alice"; // name 변수를 "Alice"로 초기화
+
+        System.out.println("이름: " + name);
+        System.out.println("금액: " + salary);
+        System.out.println("나이: " + age);
+
+    }
+}
